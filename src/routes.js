8a5c4c007466eeab866ab11a2632a2a123667fe8
fogelman <ddfogelman@gmail.com>
2019-04-28 13:42:55 -0300

    Adding route to list all

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const routes = express.Router();
 
-const ComputerController = require("./controllers/computerController");
+const ComputerController = require('./controllers/computerController');
 
 routes.post('/pc', ComputerController.store);
-routes.get('/', (req, res)=>{
-    res.send('funcionando')
-})
+routes.get('/', (req, res) => {
+  res.send('funcionando');
+});
+
+routes.get('/list', ComputerController.show);
 module.exports = routes;
