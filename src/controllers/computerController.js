60c26d4b266ab5f8eb689f3b0a4cd00f2cf9742e
fogelman <ddfogelman@gmail.com>
2019-04-28 12:58:59 -0300

    adding all

--- src/controllers/computerController.js
+++ src/controllers/computerController.js
@@ -3,6 +3,7 @@ const Computer = require('../models/computer');
 class ComputerController {
   async store(req, res) {
     const pc = await Computer.create({ body: req.body });
+    console.log(req.body);
 
     return res.json(pc);
   }
